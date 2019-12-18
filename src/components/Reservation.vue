65fe16d138e2a558d119820129b64c2ae6396f00
Svenkanheelgoedprogrammeren <truschelsven@gmail.com>
2019-12-18 12:16:21 +0100

    test

---
+++ src/components/Reservation.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Reservation
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Reservation'
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+import Orders from './components/Orders.vue'
+import Reservation from './components/Reservation.vue'
+import Signup from './components/Signup.vue'
+import Contact from './components/Contact.vue'
+import Menu from './components/Menu.vue'
+
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/orders', component: Orders },
+  { path: '/reservation', component: Reservation },
+  { path: '/signup', component: Signup },
+  { path: '/contact', component: Contact },
+  { path: '/menu', component: Menu },
+  
+  
+]
+const router = new VueRouter({
+  mode: 'history',
+  routes
+
+})
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
